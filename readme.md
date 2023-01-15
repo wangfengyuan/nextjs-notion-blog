d94d15768b7b71b27e4bad8b39d0e4b23cffaee6
wangfengyuan <644301374@qq.com>
2023-01-15 11:31:38 +0800

    feat: init project

---
+++ readme.md
@@ -0,0 +1,9 @@
+<p align="center">
+  <a href="https://transitivebullsh.it/nextjs-notion-starter-kit">
+    <img alt="Example article page" src="https://user-images.githubusercontent.com/552829/160132094-12875e09-41ec-450a-80fc-ae8cd488129d.jpg" width="689">
+  </a>
+</p>
+
+# Personal Blog
+
+A NextJS-based Personal Blog, with Notion as a CMS, A clone of [NextJS Notion Starter Kit](https://github.com/transitive-bullshit/nextjs-notion-starter-kit)
---
+++ site.config.ts
@@ -0,0 +1,65 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  // the site's root Notion page (required)
+  rootNotionPageId: '6e4c1aee7c57432baf035aee373fb317',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'Next.js Notion Starter Kit',
+  domain: 'test.wfy.blog',
+  author: 'wangfengyuan',
+
+  // open graph metadata (optional)
+  description: 'Example Next.js Notion Starter Kit Site',
+
+  // social usernames (optional)
+  // twitter: 'transitive_bs',
+  github: 'wangfengyuan',
+  // linkedin: 'fisch2',
+  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  // newsletter: '#', // optional newsletter URL
+  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: true,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null,
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+  // navigationStyle: 'default',
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '8ba18c4e306748aab894a011c77e2cbb'
+    },
+    {
+      title: 'Contact',
+      pageId: 'c4a76477ddf54262a884405b893eb4a9'
+    }
+  ]
+})
