9722379bd8be980c75cff101794683d948b1ffa8
wangfengyuan <644301374@qq.com>
2023-07-04 11:21:17 +0800

    feat: 简化flowise集成方式

--- components/ChatBot.tsx
+++ components/ChatBot.tsx
@@ -1,24 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { BubbleChat } from "flowise-embed-react";
 
 const Chatbot = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.type = "module";
-    script.src = "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js";
-    script.onload = () => {
-      (window as any).Chatbot.init({
-        chatflowid: "eddfac63-bcc5-4c3b-ab68-e1eaa2f8c5e2",
-        apiHost: "http://flowise.codefe.top",
-      });
-    };
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  return <div id="chatbot-container" />;
+  return (
+    <BubbleChat
+      chatflowid="eddfac63-bcc5-4c3b-ab68-e1eaa2f8c5e2"
+      apiHost="https://flowise.codefe.top"
+    />
+  );
 };
 
 export default Chatbot;
